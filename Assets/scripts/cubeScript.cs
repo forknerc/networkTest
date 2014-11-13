de7d9f0d37295952b0d307b5b4b003cb0750ae31
unknown <cforkner@05620396GPK02.rd.unr.edu>
2014-11-13 11:10:38 -0800

    first commit

---
+++ Assets/scripts/cubeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeScript : MonoBehaviour {
+
+	private UDPReceive netReader;
+
+	public GameObject UDPobj;
+	public UDPReceive UDPScript;
+
+	// Use this for initialization
+	void Start () {
+	
+
+		//netReader = FindObjectOfType(typeof(UDPReceive));
+		UDPobj = GameObject.Find("networkControl");
+		UDPScript = UDPobj.GetComponent<UDPReceive>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	transform.position = UDPScript.getPos();
+	
+	}
+}
